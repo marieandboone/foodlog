ce3be893c6b18302897ef562558923230cc6e5dc
marieandboone <160288419+marieandboone@users.noreply.github.com>
2024-10-04 00:13:00 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("app-cache").then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/styles.css",
+        "/script.js",
+        "/icon512_rounded.png",
+        "/icon512_maskable.png",
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
